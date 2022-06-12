641e776992ca9ac2fe0d14509184607e5a79b9ee
xBodda <abdelrahman3aysh@hotmail.com>
2022-06-12 17:18:02 +0200

    Update routes.dart

--- lib/components/routes.dart
+++ lib/components/routes.dart
@@ -3,6 +3,7 @@ import 'package:transpot/views/home.dart';
 import 'package:transpot/views/signin.dart';
 import 'package:transpot/views/signup.dart';
 import 'package:transpot/views/user/buses.dart';
+import 'package:transpot/views/user/checkout.dart';
 import 'package:transpot/views/user/find_bus.dart';
 import 'package:transpot/views/user/packages.dart';
 import 'package:transpot/views/user/payment.dart';
@@ -17,4 +18,5 @@ final Map<String, WidgetBuilder> routes = {
   Wallet.routeName: (context) => const Wallet(),
   Packages.routeName: (context) => const Packages(),
   Payment.routeName: (context) => const Payment(),
+  Checkout.routeName: (context) => const Checkout(),
 };
