532f0bff7bd05c68b9ac1260e249885dd40c1a9c
xBodda <abdelrahman3aysh@hotmail.com>
2022-06-12 17:20:14 +0200

    Cart Item Model

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class cartItem {
+  final String product;
+  final int price;
+  final String uid;
+
+  cartItem(
+      {required this.product,
+      required this.price,
+      required this.uid});
+}
