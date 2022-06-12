79f36d15cf40b3ad48da3306e6a631d5d8569048
xBodda <abdelrahman3aysh@hotmail.com>
2022-06-12 17:17:38 +0200

    On Release Keyboard Util

---
+++ lib/utils/keyboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Keyboard {
+  static void hideKeyboard(BuildContext context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
+}
